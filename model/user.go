5a9d840c5f83e9a9add1697eeab6fb5290c02c16
Nikita Loskutov <cnaize@gmail.com>
2019-02-25 23:41:27 +0400

    added basic jwt auth

--- model/user.go
+++ model/user.go
@@ -8,8 +8,9 @@ import (
 type User struct {
 	Base
 	Username  string     `json:"username" gorm:"unique_index" form:"username" binding:"required"`
-	Password1 string     `json:"-" sql:"-" form:"password1" binding:"required"`
-	Password2 string     `json:"-" sql:"-" form:"password2" binding:"required"`
+	Token     string     `json:"token" sql:"-"`
+	Password  *string    `json:"password,omitempty" sql:"-" form:"password" binding:"required"`
+	Password1 *string    `json:"password1,omitempty" sql:"-" form:"password1"`
 	PassHash  string     `json:"-" form:"-"`
 	DeletedAt *time.Time `json:"-"`
 	Error     *Error     `json:"error,omitempty" sql:"-" form:"error"`

996ab257b5ab7da3c2cb3a228e1110d1fc02ebf1
Nikita Loskutov <cnaize@gmail.com>
2019-03-02 18:42:44 +0400

    small auth fixes

--- model/user.go
+++ model/user.go
@@ -7,11 +7,8 @@ import (
 
 type User struct {
 	Base
-	Username  string     `json:"username" gorm:"unique_index" form:"username" binding:"required"`
-	Token     string     `json:"token" sql:"-"`
-	Password  *string    `json:"password,omitempty" sql:"-" form:"password" binding:"required"`
-	Password1 *string    `json:"password1,omitempty" sql:"-" form:"password1"`
-	PassHash  string     `json:"-" form:"-"`
+	Username  string     `json:"username" gorm:"unique_index"`
+	PassHash  string     `json:"-"`
 	DeletedAt *time.Time `json:"-"`
 	Error     *Error     `json:"error,omitempty" sql:"-" form:"error"`
 }
